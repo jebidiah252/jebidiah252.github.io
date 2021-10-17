b4b4075592a5c75042cea76eb627dac48fdec36d
Gordon Fjeldsted <gordon.fjeldsted@gmail.com>
2021-10-17 11:38:08 -0600

    quick fixes based on assignment description

--- resources/scripts/calculator.js
+++ resources/scripts/calculator.js
@@ -26,3 +26,14 @@ function printCalculations(calculation) {
     console.log("Value of op: " + calculation.getOp());
     console.log("Value of Y: " + calculation.getY());
 }
+
+const calculations = [];
+do {
+    let x = prompt("What is the value of 'x'?");
+    let op = prompt("What operator will you be using?");
+    let y = prompt("What is the value of 'y'?");
+
+    calculations.push(new Calculation(x, y, op));
+} while (confirm("Would you like to continue?"));
+
+calculations.forEach(printCalculations);
